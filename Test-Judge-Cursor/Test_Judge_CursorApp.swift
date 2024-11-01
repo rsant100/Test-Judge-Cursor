bf0b2b00ea4cd83cd02e80996e4a6cfa7608b3eb
rsant100 <36575875+rsant100@users.noreply.github.com>
2024-11-01 13:44:21 -0500

    Initial Commit

---
+++ Test-Judge-Cursor/Test_Judge_CursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Judge_CursorApp.swift
+//  Test-Judge-Cursor
+//
+//  Created by Robert Santini on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_Judge_CursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
